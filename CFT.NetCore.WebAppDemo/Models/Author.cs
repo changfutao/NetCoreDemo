b75abeb63ab075d4636c8388777b394da0b3d8c8
friends <changfutao1989@126.com>
2020-04-28 07:37:15 +0800

    asd

---
+++ CFT.NetCore.WebAppDemo/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CFT.NetCore.WebAppDemo.Models
+{
+    public class Author
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset BirthDate { get; set; }
+        public string BirthPlace { get; set; }
+        public string Email { get; set; }
+    }
+}
